d57484e44191e08facc27b448817343ddd952217
bholuh <Bholuh06@gmail.com>
2022-10-14 16:31:14 +0100

    Task 2 Models

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,29 @@ from datetime import datetime
 class BaseModel:
     """A base class for all hbnb models"""
 
+from datetime import datetime
+import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
+
+
+class BaseModel:
+    """The BaseModel class from which future classes will be derived"""
+    if models.storage_t == "db":
+        id = Column(String(60), primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
