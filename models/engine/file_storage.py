6fada21c843be13cd6530d4a85657dc7f2e23de2
bholuh <Bholuh06@gmail.com>
2022-10-14 16:17:47 +0100

    Task 2 Models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,17 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 
+import json
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -35,6 +46,10 @@ class FileStorage:
         from models.amenity import Amenity
         from models.review import Review
 
+def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
+        
         classes = {
             'BaseModel': BaseModel, 'User': User, 'Place': Place,
             'State': State, 'City': City, 'Amenity': Amenity,
